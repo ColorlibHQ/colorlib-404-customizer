b118bdd6950847cdd46aba905f47ed5b40682077
razvanaldea89 <razvan.aldea89@yahoo.ro>
2018-11-27 14:56:59 +0200

    variable naming fix

--- includes/controls/class-cnfp-template-selection.php
+++ includes/controls/class-cnfp-template-selection.php
@@ -12,19 +12,19 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 			 * Render the control in the customizer
 			 */
 			public function render_content() {
-				$ccsm_options = get_option( 'cnfp_settings' );
-				$template     = $ccsm_options['colorlib_404_customizer_select_template'];
+				$cnfp_options = get_option( 'cnfp_settings' );
+				$template     = $cnfp_options['colorlib_404_customizer_select_template'];
 				?>
                 <div class="colorlib_template_selection_radio">
                     <div class="colorlib-templates-wrapper">
 						<?php foreach ( $this->choices as $key => $value ) { ?>
-                            <?php
+							<?php
 							if ( $key == $template ) {
 								$active = 'active';
 							} else {
 								$active = '';
 							}
-                            ?>
+							?>
                             <label class="colorlib-single-template-wrapper <?php echo $active; ?>">
                                 <img
                                         src="<?php echo CNFP_URL . 'templates/' . esc_attr( $key ) . '/' . esc_attr( $key ) . '.png' ?>">
